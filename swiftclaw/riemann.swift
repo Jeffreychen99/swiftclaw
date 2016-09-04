fb0fad099c554f0d0f18b748c60ee799d1d01083
Jeffreychen99 <personjeffrey@gmail.com>
2016-09-04 16:56:08 -0400

    Create riemann.swift

---
+++ swiftclaw/riemann.swift
@@ -0,0 +1,199 @@
+//
+//  riemann.swift
+//  swiftclaw
+//
+//  Created by Jeffrey Chen on 8/10/16.
+//  Copyright (c) 2016 Jeffrey Chen. All rights reserved.
+//
+
+import Foundation
+import Darwin
+
+struct arrayGroup
+{
+    var wave: [[[Double]]]
+    var s: [[Double]]
+    var amdq: [[Double]]
+    var apdq: [[Double]]
+    
+    init(aTemp: [[[Double]]], bTemp: [[Double]], cTemp: [[Double]], dTemp: [[Double]])
+    {
+        wave = aTemp
+        s = bTemp
+        amdq = cTemp
+        apdq = dTemp
+    }
+}
+
+
+func advection_1d(q_l: [[Double]], q_r: [[Double]], aux_l: [Double], aux_r: [Double], problem_data: [String: Double]) -> arrayGroup
+{
+    let num_eqn = 1
+    let num_waves = 1
+    
+    // Number of Riemann Problems we are solving
+    let num_rp = q_l[0].count
+    
+    // Return values
+    var wave = [[[Double]]](count: num_eqn, repeatedValue: [[Double]](count: num_waves, repeatedValue: [Double](count: num_rp, repeatedValue: Double())))
+    var s = [[Double]](count: num_eqn, repeatedValue: [Double](count: num_rp, repeatedValue: Double()))
+    var amdq = [[Double]](count: num_eqn, repeatedValue: [Double](count: num_rp, repeatedValue: 0.0))
+    var apdq = [[Double]](count: num_eqn, repeatedValue: [Double](count: num_rp, repeatedValue: 0.0))
+    
+    for i in 0 ..< wave[0][0].count
+    {
+        wave[0][0][i] = q_r[0][i] - q_l[0][i]
+        s[0][i] = problem_data["u"]!
+    }
+    
+    if(problem_data["u"] > 0)
+    {
+        for i in 0 ..< apdq[0].count
+        {
+            apdq[0][i] = s[0][i] * wave[0][0][i]
+        }
+    }
+    else
+    {
+        for i in 0 ..< amdq[0].count
+        {
+            amdq[0][i] = Double(s[0][i]) * wave[0][0][i]
+        }
+    }
+    
+    return arrayGroup(aTemp: wave, bTemp: s, cTemp: amdq, dTemp: apdq)
+}
+
+
+
+
+
+
+func acoustics_1d(q_l: [[Double]], q_r: [[Double]], aux_l: [Double], aux_r: [Double], problem_data: [String: Double]) -> arrayGroup
+{
+    let num_eqn = 2
+    let num_waves = 2
+    
+    // Number of Riemann Problems we are solving
+    let num_rp = q_l[0].count
+    
+    // Return values
+    var wave = [[[Double]]](count: num_eqn, repeatedValue: [[Double]](count: num_waves, repeatedValue: [Double](count: num_rp, repeatedValue: Double())))
+    var s = [[Double]](count: num_eqn, repeatedValue: [Double](count: num_rp, repeatedValue: Double()))
+    var amdq = [[Double]](count: num_eqn, repeatedValue: [Double](count: num_rp, repeatedValue: Double()))
+    var apdq = [[Double]](count: num_eqn, repeatedValue: [Double](count: num_rp, repeatedValue: Double()))
+    
+    // Local values
+    var delta = [[Double]](count: q_r.count, repeatedValue: [Double](count: q_r[0].count, repeatedValue: Double()))
+    for i in 0 ..< delta.count
+    {
+        for j in 0 ..< delta[0].count
+        {
+            delta[i][j] = q_r[i][j] - q_l[i][j]
+        }
+    }
+    
+    var a1 = [Double](count: delta[0].count, repeatedValue: Double())
+    var a2 = [Double](count: delta[0].count, repeatedValue: Double())
+
+    for i in 0 ..< a1.count
+    {
+        a1[i] = (-delta[0][i] + problem_data["zz"]!*delta[1][i]) / (2.0 * problem_data["zz"]!)
+        a2[i] = (delta[0][i] + problem_data["zz"]!*delta[1][i]) / (2.0 * problem_data["zz"]!)
+    }
+    
+    // Compute the waves
+    // 1-Wave
+    for i in 0 ..< wave[0][0].count
+    {
+        wave[0][0][i] = -a1[i] * problem_data["zz"]!
+        wave[1][0][i] = a1[i]
+        s[0][i] = -problem_data["cc"]!
+    }
+    
+    // 2-Wave
+    for i in 0 ..< wave[0][0].count
+    {
+        wave[0][1][i] = -a2[i] * problem_data["zz"]!
+        wave[1][1][i] = a2[i]
+        s[1][i] = -problem_data["cc"]!
+    }
+    
+    // Compute the left going and right going fluctuation
+    for m in 0 ..< num_eqn
+    {
+        for i in 0 ..< amdq[0].count
+        {
+            amdq[m][i] = s[0][i] * wave[m][0][i]
+            apdq[m][i] = s[1][i] * wave[m][1][i]
+        }
+    }
+    
+    return arrayGroup(aTemp: wave, bTemp: s, cTemp: amdq, dTemp: apdq)
+}
+
+
+
+
+
+
+func burgers_1d(q_l: [[Double]], q_r: [[Double]], aux_l: [Double], aux_r: [Double], problem_data: [String: Double]) -> arrayGroup
+{
+    let num_eqn = 1
+    let num_waves = 1
+    
+    // Number of Riemann Problems we are solving
+    let num_rp = q_l[0].count
+    
+    // Output arrays
+    var wave = [[[Double]]](count: num_eqn, repeatedValue: [[Double]](count: num_waves, repeatedValue: [Double](count: num_rp, repeatedValue: Double())))
+    var s = [[Double]](count: num_eqn, repeatedValue: [Double](count: num_rp, repeatedValue: Double()))
+    var amdq = [[Double]](count: num_eqn, repeatedValue: [Double](count: num_rp, repeatedValue: Double()))
+    var apdq = [[Double]](count: num_eqn, repeatedValue: [Double](count: num_rp, repeatedValue: Double()))
+    
+    // Basic solve
+    for i in 0 ..< wave[0].count
+    {
+        for j in 0 ..< wave[0][i].count
+        {
+            wave[0][i][j] = q_r[i][j] - q_l[i][j]
+        }
+    }
+    for i in 0 ..< s[0].count
+    {
+        s[0][i] = 0.5 * (q_r[0][i]+q_l[0][i])
+    }
+    
+    var s_index = [[Double]](count: 2, repeatedValue: [Double](count: num_rp, repeatedValue: 0.0))
+    for i in 0 ..< s[0].count
+    {
+        s_index[0][i] = s[0][i]
+    }
+    
+    for i in 0 ..< wave[0][0].count
+    {
+        if(s_index[0][i] < 0.0)
+        {
+            amdq[0][i] = s_index[0][i] * wave[0][0][i]
+            apdq[0][i] = 0.0
+        }
+        else
+        {
+            amdq[0][i] = 0.0
+            apdq[0][i] = s_index[0][i] * wave[0][0][i]
+        }
+    }
+    
+    // Compute entropy fix
+    if(problem_data["efix"] == 1.0)
+    {
+        //var transonic = [
+    }
+    
+    
+    return arrayGroup(aTemp: wave, bTemp: s, cTemp: amdq, dTemp: apdq)
+}
+
+
+
+
